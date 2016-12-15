2f2ad73e1b6d828e450464907139f53620c90f13
Jon Medding <jonmedding@gmail.com>
2016-12-15 06:18:12 +0100

    Updated Sensor version

--- apps/fmeister/mix.exs
+++ apps/fmeister/mix.exs
@@ -33,7 +33,7 @@ defmodule Fmeister.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [ {:sensor, "~>0.1.6"},
+    [ {:sensor, "~>0.1.7"},
       {:temp_control, in_umbrella: true}
     ]
   end
